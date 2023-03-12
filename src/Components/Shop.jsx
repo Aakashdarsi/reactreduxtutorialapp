37a98faac4bab08e588293a6247893cb8ec7ee8c
Darsi Sriguru Aakash <66508746+Aakashdarsi@users.noreply.github.com>
2023-03-12 09:50:05 +0530

    Redux Snapshot 1

---
+++ src/Components/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <div className='container text-center'>
+        <h2>Buy Addidas Shoes $50</h2>
+        <button className="btn btn-primary mx-2">+</button>
+        Add This item to cart
+        <button className="btn btn-primary mx-2">-</button>
+
+    </div>
+  )
+}
